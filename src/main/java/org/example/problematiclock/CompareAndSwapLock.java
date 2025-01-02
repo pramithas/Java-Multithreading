bc08bc3f943b4f75e1c868eff1d347b7b5d23dcb
pramithas123 <pramithas@aruna.software>
2025-01-02 10:08:19 +0545

    Add compare and swap class

--- src/main/java/org/example/problematiclock/CompareAndSwapLock.java
+++ src/main/java/org/example/problematiclock/CompareAndSwapLock.java
@@ -1,4 +1,18 @@
 package org.example.problematiclock;
 
-public class CompareAndSwapLock {
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class CompareAndSwapLock{
+
+    private AtomicBoolean atomicLocked = new AtomicBoolean(false);
+
+    public void unlock() {
+        this.atomicLocked.set(false);
+    }
+
+    public void lock() {
+        while(!this.atomicLocked.compareAndSet(false, true)) {
+            //busy wait.
+        }
+    }
 }
